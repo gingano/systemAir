f82253c03eff3c2e9e0a77e213a687757c5756a8
Oleksii Kovalenko <o.kovalenko@pozitiff.ua>
2020-02-13 19:13:56 +0200

    map update

--- JavaScript/mapChart.js
+++ JavaScript/mapChart.js
@@ -1,13 +1,173 @@
+let currentCountry;
+let countryDescription = document.querySelector('.country-description');
+
 let chart = am4core.create("chartdiv", am4maps.MapChart);
-let currentCountriesArray = ["FI", "DK", "SE", "NO", "LT", "LV", "EE", "IS", 'UA'];
+let currentCountriesArray = [];
 let currentCountriesData = [{
   "id": "UA",
-  "value": 100
+  "value": [
+    'Fans',
+    "Residential ventilation",
+    "Swimming pool ventilation",
+    "Production of Air handling units",
+    "Air conditioning",
+    "Air curtains and heating products",
+    "Products for air distribution",
+    "Fire safety",
+    "Garage and tunnel ventilation",
+    "Distributions centres",
+    "Sales",
+    "Technology centres"
+  ]
 }, {
   "id": "FR",
-  "name": "France",
-  "value": 50
+  "value": [
+    'Fans',
+    "Residential ventilation",
+    "Swimming pool ventilation",
+    "Production of Air handling units",
+    "Air conditioning",
+    "Air curtains and heating products",
+    "Distributions centres",
+    "Technology centres"
+  ]
+}, {
+  "id": "FI",
+  "value": [
+    'Fans',
+    "Residential ventilation",
+    "Swimming pool ventilation",
+    "Production of Air handling units",
+    "Air conditioning",
+    "Air curtains and heating products",
+    "Distributions centres",
+    "Technology centres"
+  ]
+}, {
+  "id": "DK",
+  "value": [
+    'Fans',
+    "Residential ventilation",
+    "Swimming pool ventilation",
+    "Production of Air handling units",
+    "Air conditioning",
+    "Air curtains and heating products",
+    "Products for air distribution",
+    "Fire safety",
+    "Garage and tunnel ventilation",
+    "Distributions centres",
+    "Sales",
+    "Technology centres"
+  ]
+}, {
+  "id": "SE",
+  "value": [
+    'Fans',
+    "Residential ventilation",
+    "Swimming pool ventilation",
+    "Production of Air handling units",
+    "Air conditioning",
+    "Air curtains and heating products",
+    "Distributions centres",
+    "Technology centres"
+  ]
+}, {
+  "id": "NO",
+  "value": [
+    'Fans',
+    "Residential ventilation",
+    "Swimming pool ventilation",
+    "Production of Air handling units",
+    "Air conditioning",
+    "Air curtains and heating products",
+    "Distributions centres",
+    "Technology centres"
+  ]
+}, {
+  "id": "LT",
+  "value": [
+    'Fans',
+    "Residential ventilation",
+    "Swimming pool ventilation",
+    "Production of Air handling units",
+    "Air conditioning",
+    "Air curtains and heating products",
+    "Products for air distribution",
+    "Fire safety",
+    "Garage and tunnel ventilation",
+    "Distributions centres",
+    "Sales",
+    "Technology centres"
+  ]
+}, {
+  "id": "LV",
+  "value": [
+    'Fans',
+    "Residential ventilation",
+    "Swimming pool ventilation",
+    "Production of Air handling units",
+    "Air conditioning",
+    "Air curtains and heating products",
+    "Distributions centres",
+    "Technology centres"
+  ]
+}, {
+  "id": "EE",
+  "value": [
+    'Fans',
+    "Residential ventilation",
+    "Swimming pool ventilation",
+    "Production of Air handling units",
+    "Air conditioning",
+    "Air curtains and heating products",
+    "Distributions centres",
+    "Technology centres"
+  ]
+}, {
+  "id": "IS",
+  "value": [
+    'Fans',
+    "Residential ventilation",
+    "Swimming pool ventilation",
+    "Production of Air handling units",
+    "Air conditioning",
+    "Air curtains and heating products",
+    "Products for air distribution",
+    "Fire safety",
+    "Garage and tunnel ventilation",
+    "Distributions centres",
+    "Sales",
+    "Technology centres"
+  ]
+}, {
+  "id": "GB",
+  "value": [
+    'Fans',
+    "Residential ventilation",
+    "Swimming pool ventilation",
+    "Production of Air handling units",
+    "Air conditioning",
+    "Air curtains and heating products",
+    "Distributions centres",
+    "Technology centres"
+  ]
+}, {
+  "id": "DE",
+  "value": [
+    'Fans',
+    "Residential ventilation",
+    "Swimming pool ventilation",
+    "Production of Air handling units",
+    "Air conditioning",
+    "Air curtains and heating products",
+    "Distributions centres",
+    "Technology centres"
+  ]
 }]
+
+currentCountriesData.forEach(function (object) {
+  currentCountriesArray.push(object.id)
+})
 
 chart.geodata = am4geodata_worldLow;
 chart.projection = new am4maps.projections.Mercator();
@@ -21,17 +181,26 @@ polygonSeries.exclude = ["AQ"];
 let currentCountries = chart.series.push(new am4maps.MapPolygonSeries());
 currentCountries.useGeodata = true;
 currentCountries.include = currentCountriesArray;
-currentCountries.mapPolygons.template.tooltipHTML = "<h3>{name}</h3>";
 currentCountries.mapPolygons.template.fill = am4core.color("#00a7d8");
 currentCountries.data = currentCountriesData
 
-// let currentCountry;
-// currentCountries.mapPolygons.template.events.on("hit", function(e) {
-//   currentCountry = e.target;
-//   currentCountry.tooltipHTML = "<h3>{name}</h3>";
-//   currentCountry.showTooltip();
-//   console.log(currentCountry.name)
-// });
+currentCountries.mapPolygons.template.events.on("hit", function(e) {
+  currentCountry = e.target.dataItem.dataContext;
+
+  countryDescription.classList.add('country-description-active')
+  countryDescription.style.top = e.point.y + 'px';
+  countryDescription.style.left = e.point.x + 'px';
+
+  countryDescription.innerHTML = '' +
+    '<h3>' + currentCountry.name + '</h3>' +
+    '<ul class="description-list"></ul>'
+
+  currentCountry.value.forEach(function (value) {
+    let listItem = document.createElement('li');
+    listItem.innerHTML = value
+    document.querySelector('.description-list').append(listItem);
+  })
+});
 
 let hs = currentCountries.mapPolygons.template.states.create("hover");
 hs.properties.fill = am4core.color("#004985");
